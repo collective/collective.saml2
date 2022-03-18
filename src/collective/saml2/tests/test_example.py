ffacb0a3b402bc67e75953220c71c20633b2fed6
Dylan Jay <software@pretaweb.com>
2022-03-18 09:14:43 +0700

    flake8 fixes

--- src/collective/saml2/tests/test_example.py
+++ src/collective/saml2/tests/test_example.py
@@ -23,4 +23,3 @@ class TestExample(unittest.TestCase):
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
-
